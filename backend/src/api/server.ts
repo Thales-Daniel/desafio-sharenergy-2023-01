61407d21eb5fec034ac95d9d1be0403064ec3d10
Thales <thalesdaniel2013@gmail.com>
2023-01-10 14:17:20 -0300

    chore: make some changes in package.json

--- backend/src/api/server.ts
+++ backend/src/api/server.ts
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 
 const app = express()
-const PORT = process.env.PORT || 3333
+const PORT = process.env.PORT || 8000
 
 app.use(express.json())
 app.use(cors())
